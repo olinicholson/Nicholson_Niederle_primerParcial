f1db86d7e90ce50b72c5bbcf930de89a0b0233c9
olinicholson <olinicholson4@gmail.com>
2023-04-25 12:19:43 -0300

    con el ultimo grafico

--- scripts/chart_4.js
+++ scripts/chart_4.js
@@ -1,27 +1,21 @@
 d3.dsv(";",'../datos/147_vehiculos_mal_estacionados_aux3.csv', d3.autoType).then(data => {
     console.log(data)
     // filtramos los barrios necesarios
-    let data_aux = data.filter( item => 
-      item.domicilio_barrio == 'PALERMO' )
-    let chart4 = Plot.plot({
+    let data_palermo = data.filter( item => 
+      (item.domicilio_barrio == 'PALERMO') && (item.estado_del_contacto != null)&& (item.subcategoria != "DEFENSA AL CONSUMIDOR")&& (item.subcategoria != "OTROS"))
+    let data_soldati = data.filter( item => 
+      (item.domicilio_barrio == 'VILLA SOLDATI')&& (item.estado_del_contacto != null) )
+    let data_aux= data.filter( item => 
+      item.domicilio_barrio == 'PALERMO' ||'VILLA SOLDATI'  )
+
+    let chartPalermo = Plot.plot({
       marks: [
-        Plot.barX(data_aux, 
+        Plot.barX(data_palermo, 
           Plot.groupY({ 
             x:"count" }, 
           { 
             y: "subcategoria", 
-            fill: "genero"
-            /*function(d){
-                if (d.genero=="mujer"){
-                    return color; 
-                }
-                else  if (d.genero=="null"){
-                    return color; 
-                }
-                if (d.genero=="hombre"){
-                    return color; 
-                }
-            }*/
+            fill: "estado_del_contacto"
         }) ),
         
         ],
@@ -33,15 +27,43 @@ d3.dsv(";",'../datos/147_vehiculos_mal_estacionados_aux3.csv', d3.autoType).then
         x: {
           label: "CANTIDAD DE DENUNCIAS",
           line: true,
+          domain: [0,1000],
         },
-        
         color: {
           legend: true,
-          
         },
         marginLeft: 150,
-    
     })
-    d3.select('#chart_4').append(() => chart4)
+
+    d3.select('#chart_4').append(() => chartPalermo)
+
+    
+    let chartSoldati = Plot.plot({
+      marks: [
+        Plot.barX(data_soldati, 
+          Plot.groupY({ 
+            x:"count" }, 
+          { 
+            y: "subcategoria", 
+            fill: "estado_del_contacto"
+        }) ),
+        
+        ],
+        y: {
+          line: true,
+          tickFormat: d => d.toLowerCase().replace(/\b\w/g, l => l.toUpperCase()),
+          
+    
+        },
+        x: {
+          label: "CANTIDAD DE DENUNCIAS",
+          line: true,
+          domain: [0,1000],
+        },
+    
+        marginLeft: 150,
+    })
+
+    d3.select('#chart_4').append(() => chartSoldati)
   })
   
